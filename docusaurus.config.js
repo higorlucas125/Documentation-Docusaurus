2a0f6b5c64d1d7aba7dfdfc94b8f19af512812c2
higorlucas125 <higorlaf@hotmail.com>
2025-11-08 01:22:11 -0300

    feat: new information in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,6 +16,8 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs-as-code-tutorial/',
 
+  deploymentBranch: 'gh-pages',
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'higor lucas', // Usually your GitHub org/user name.
